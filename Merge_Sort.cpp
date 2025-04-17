4bec1e44cb5b323f1e18cdb5727e48ec1c6d4790
RifalArdi25 <rifalardi25@gmail.com>
2025-04-17 10:45:32 +0700

    Menambahkan Prosedur Input

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -3,3 +3,30 @@ using namespace std;
 
 int arr[20], B[20]; // Membuat Array Utama dan Temporary Array
 int n; // Membuat Variabel Input n 
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n-------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
